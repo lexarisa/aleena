84c5eb497bdf8dc7b8e3460a9aa1f868ecca33c1
Alexandra Plana <alexandraplanarodon@gmail.com>
2022-05-09 18:56:47 +0200

    basic sse

--- client/common/components/Feed.tsx
+++ client/common/components/Feed.tsx
@@ -1,43 +1,34 @@
-import { connected } from 'process';
 import React, { useEffect, useState } from 'react';
 
 function Feed() {
-  const [tasks, setTasks] = useState([]);
-  useEffect(() => {
-    //fetch from db
+  // const [tasks, setTasks] = useState([]);
+  // useEffect(() => {
+  //   //fetch from db
 
-    //SSE
-    const sse = new EventSource('SSE_URL');
-    //on error
-    sse.onerror = () => {
-      console.log('server closed connection');
-      sse.close();
-    };
+  //   //SSE
+  //   const sse = new EventSource('/feed');
+  //   //on error
+  //   sse.onerror = () => {
+  //     console.log('server closed connection');
+  //     sse.close();
+  //   };
 
-    function getRealtimeData(data) {
-      // process the data here
-      let newTask = data;
-      // pass it to state to be rendered
-      setTasks(() => [...tasks, newTask]);
-    }
-    sse.onmessage = (e) => getRealtimeData(JSON.parse(e.data));
-    sse.close();
-  }, []);
+  //   function getRealtimeData(data) {
+  //     // process the data here
+  //     let newTask = data;
+  //     // pass it to state to be rendered
+  //     setTasks(() => [...tasks, newTask]);
+  //   }
+  //   sse.onmessage = (e) => getRealtimeData(JSON.parse(e.data));
+  //   sse.close();
+  // }, []);
+  const source = new EventSource('/feed');
+
+  source.addEventListener('message', (message) => {
+    console.log('Got', message);
+  });
 
   return <div>Feed</div>;
 }
 
 export default Feed;
-
-app.get('/feed', (req, res) => {
-  console.log('client connected');
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  //send data
-  res.write(`data:${'data to send'}\n`);
-  res.on('close', () => {
-    console.log('client closed connection');
-    res.end();
-  });
-});
