1932ec558fe50f309e83f362d3a6ec14a7007799
thaistcosta <thais_costa31@hotmail.com>
2022-05-10 22:20:21 +0200

    merge

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
+import Login from './../common/components/Login'
 import styles from '../styles/login.module.css';
 
-const login = () => {
+const LoginPage = () => {
+
+
   return (
-    <div className={styles.container}>
-      <div className={styles.form}>
-        <form action=" ">
-          <div className={styles.logo}>Alena</div>
-          <h1 className={styles.header}>Login</h1>
-          <button className={styles.github}>Log in with Github</button>
-        </form>
-      </div>
-    </div>
+    <Login></Login>
   );
 };
 
-export default login;
+export default LoginPage;
