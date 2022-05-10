a86c60329c219113b411d7e27070da75c98fb629
lexarisa <lexarisaa@gmail.com>
2022-05-10 22:38:19 +0200

    make some ITask fields optional

--- client/common/types/ITask.ts
+++ client/common/types/ITask.ts
@@ -3,15 +3,15 @@ import IUser from './IUser';
 export default interface ITask {
   id: Number;
   title: String;
-  users: String[]; //IUser[];
-  description: String;
-  status: String;
-  priority: String;
-  tags: String[]; //Tags[]
-  deadline: String;
-  milestone_id: String;
-  comments: String[]; // Comment[]
-  githubs: String[]; // Github[]
+  users?: String[]; //IUser[];
+  description?: String;
+  status?: String;
+  priority?: String;
+  tags?: String[]; //Tags[]
+  deadline?: String;
+  milestone_id?: String;
+  comments?: String[]; // Comment[]
+  githubs?: String[]; // Github[]
   created_at: String; //Date;
-  updated_at: String; //Date;
+  updated_at?: String; //Date;
 }
---
+++ client/pages/api/dashBoardApi.ts
@@ -0,0 +1,8 @@
+const URL = 'http://localhost:3001';
+export const getServerSideProps = async (context) => {
+  // const res = await fetch(`${URL}/dashboard/${project_id}/${user_id}/${page}`);
+
+  return {
+    props: {},
+  };
+};
