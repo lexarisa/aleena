76c3d1d01d16d4be842e786100f5db340bdfe796
thaistcosta <thais_costa31@hotmail.com>
2022-05-11 12:29:30 +0200

    changed the controllers of feed and deleted files
    duplicated files

--- server/src/routers/project.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import express from 'express';
-import projectController from '../controllers/project.controller';
-
-const projectRouter: Router = express.Router();
-
-projectRouter.get('/project/:projectId', projectController.selectProject);
-
-export default projectRouter;
