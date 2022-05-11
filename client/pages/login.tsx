b419a276993c8f08b34bd4cb53fb77799bb30802
lexarisa <lexarisaa@gmail.com>
2022-05-11 11:35:29 +0200

    solved conflicts

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,31 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import Login from '../common/components/Login';
 
 const login = () => {
   return <Login />;
-=======
-import Login from '../common/components/Login';
-import styles from '../styles/login.module.css';
-import Login from '../common/components/Login';
-
-const login = () => {
-  return (
-    // <div className={styles.container}>
-    //   <div className={styles.form}>
-
-    //     {/* <form action=" ">
-    //       <div className={styles.logo}>Alena</div>
-    //       <h1 className={styles.header}>Login</h1>
-    //       <button className={styles.github}>Log in with Github</button>
-    //     </form> */}
-
-
-    //   </div>
-    // </div>
-    <Login />
-  );
->>>>>>> development
 };
 
 export default login;
