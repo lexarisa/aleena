e4752a0cdcd51fab5283f3c9865c5d149ee295fb
Alexandra Plana <alexandraplanarodon@gmail.com>
2022-05-17 12:53:41 +0200

    redux working on docs and articles

--- server/src/models/Documentation/documentation.routes.ts
+++ server/src/models/Documentation/documentation.routes.ts
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import { DocumentationController } from './documentation.controllers';
 const router: Router = Router();
 const controller = new DocumentationController();
+
 router.get('/documentation/sse', controller.documentationSSE);
+router.get('/documentation/:milestoneId', controller.getAllDocsInMilestone);
 router.post('/documentation', controller.createDocumentation);
 router.patch('/documentation', controller.updateDocumentation);
 router.delete('/documentation', controller.deleteDocumentation);
-
-router.get('/documentation/:milestoneId', controller.getAllDocsInMilestone);
 
 export default router;
