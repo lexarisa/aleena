ccf84762d44669673ef5045347b0f84172f806f7
lexarisa <lexarisaa@gmail.com>
2022-05-19 02:42:53 +0200

    cleaned login page

--- client/common/components/Login.tsx
+++ client/common/components/Login.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import styles from '../../styles/Login.module.css';
+import styles from '../../styles/login.module.css';
 // import { useSession, signOut } from 'next-auth/react';
 import CustomButton from './small/CustomButton';
+import { AiFillGithub } from 'react-icons/ai';
 
 const Login = () => {
   // const { data, status } = useSession()
@@ -28,22 +29,35 @@ const Login = () => {
     <>
       <div className={styles.container}>
         <div className={styles.cardWrapper}>
-          <div>
-            <h1>Login to Alena</h1>
+          <div className={styles.header}>
+            <h1 className={styles.heading}>Log In to Aleena</h1>
           </div>
           <div className={styles.login}>
-            <CustomButton
-              button="Login with Github"
-              onClick={handleSignIn}
-              color="#415a77"
-              textColor="#fff"
-            />
-            <CustomButton
-              button="Sign Up"
-              onClick={handleSignUp}
-              color="#e0e1dd"
-              textColor="#191919"
-            />
+            <button className={styles.github} onClick={handleSignIn}>
+              <AiFillGithub className={styles.icon} />
+              Login with Github
+            </button>
+            <button className={styles.signup} onClick={handleSignIn}>
+              Signup with Github
+            </button>
+
+            <div className={styles.loginOption}>
+              <label htmlFor="email" className={styles.label}>
+                Email Address
+              </label>
+              <input
+                type="text"
+                placeholder="name@company.com"
+                className={styles.input}
+              />
+              <CustomButton
+                button="Continue"
+                onClick={handleSignUp}
+                color="#415a77"
+                textColor="#f5f5f5"
+              />
+            </div>
+            <p className={styles.notice}>Terms of Service and Privacy </p>
           </div>
         </div>
       </div>
--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import Meta from '../common/components/Meta';
 import Login from './../common/components/Login';
 
 const LoginPage = () => {
   return (
     <div>
+      <Meta
+        title="Login to Aleena"
+        keywords="Aleena"
+        description="Productivity tool for developers"
+      />
       <Login />
     </div>
   );
